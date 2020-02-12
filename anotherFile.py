b0f23bb06872cf2b79fd0b2fbed0df4d53fb0296
Lewis <mlewis8@RVLAS033L-03741.lewisu.local>
2020-02-12 12:10:36 -0600

    Adding anotherFile.py

---
+++ anotherFile.py
@@ -0,0 +1,19 @@
+# This is my version of newFile.py
+
+
+// comments
+while(true){
+    print("Testing");
+}
+
+a = [ 125, 5, 7]
+for i in a :
+	print(i)
+
+b = [ 2, 56]
+
+c = [ 2233, 2256, 765]
+
+for(int i = 0; i < c.length; i++){
+
+}
