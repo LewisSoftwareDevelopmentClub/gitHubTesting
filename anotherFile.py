ed7711d0320c80dd294dd8f52493c0cfe827f791
Lewis <mlewis8@RVLAS033L-03741.lewisu.local>
2020-02-26 15:28:37 -0600

    Updates

--- anotherFile.py
+++ anotherFile.py
@@ -6,13 +6,7 @@ while(true){
     print("Testing");
 }
 
-
-
 a = [ 125, 5, 7]
-
-
-a = [ 125, 5, 7]
-
 for i in a :
 	print(i)
 
@@ -20,10 +14,6 @@ b = [ 2, 56]
 
 c = [ 2233, 2256, 765]
 
-
 for(int i = 0; i < c.length; i++){
 
-	# Implementing more code here on branch 1
-
 }
-
